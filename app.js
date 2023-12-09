3c32a5597142d50a339012f454e919271079fe28
Nel <nel.renaudin@gmail.com>
2023-12-09 13:21:54 +0100

    Enable cross origin requests

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 // Connect to database
 const mongoDB = process.env.MONGODB_URI
