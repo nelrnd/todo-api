ffad4602d8ee003fe0c6db8443c0e2eb5e59d85c
Nel <nel.renaudin@gmail.com>
2023-12-15 17:20:33 +0100

    Prepare for deployment

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const compression = require("compression")
+const helmet = require("helmet")
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
+app.use(compression())
+app.use(helmet())
+
+// Set up rate limiter
+const RateLimit = require("express-rate-limit")
+const limiter = RateLimit({ windowsMs: 1 * 60 * 1000, max: 100 })
+app.use(limiter)
 
 // Connect to database
 const mongoDB = process.env.MONGODB_URI
